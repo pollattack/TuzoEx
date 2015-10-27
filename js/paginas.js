53ea6757777b349e502c138f16dc23f2f28ada0f
Pablo Lucas <pollattack@gmail.com>
2015-10-27 12:16:13 -0600

    Propuesta de página Tuzo Express

---
+++ js/paginas.js
@@ -0,0 +1,53 @@
+$(function abrir(){
+   $("#quienes_somos").click(function(){
+    $("#contenido").load("quienes_somos.html"); 
+   });
+});
+
+$(function abrir(){
+   $("#productos").click(function(){
+    $("#contenido").load("secciones.html"); 
+   });
+});
+
+$(function abrir(){
+   $("#servicios").click(function(){
+    $("#contenido").load("servicios.html"); 
+   });
+});
+
+
+$(function abrir(){
+   $("#consume").click(function(){
+    $("#contenido").load("consume.html"); 
+   });
+});
+
+$(function abrir(){
+   $("#proveedor").click(function(){
+    $("#contenido").load("proveedores.html"); 
+   });
+});
+
+$(function abrir(){
+   $("#sucursales").click(function(){
+    $("#contenido").load("sucursales.html"); 
+   });
+});
+
+$(function abrir(){
+   $("#forma").click(function(){
+    $("#contenido").load("forma_parte.html"); 
+   });
+});
+
+$(function abrir(){
+   $("#proveedor2").click(function(){
+    $("#contenido").load("proveedor_2.html"); 
+   });
+});
+
+
+
+
+
